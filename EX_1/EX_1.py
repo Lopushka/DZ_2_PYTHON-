2e85bb4f890e820e848f014b5488625d0304e5d6
Lopushka <sv13yat@gmail.com>
2023-03-14 19:53:53 +0300

    EX_1_fin_V.1

--- EX_1/EX_1.py
+++ EX_1/EX_1.py
@@ -1,1 +1,7 @@
-n= int(input())
+m = int(input('Введите кол-во монет: '))
+cnt = 0
+for i in range(m):
+    p = int(input(f'Положение монеты {i+1}: 0 - это решка или 1 - это гербом ? '))
+    if p == 0:
+        cnt += 1
+print(f'Кол-во монет, надо перевернуть: {cnt if cnt<m/2 else m-cnt}')
